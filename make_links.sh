33e1855d932b5a1d311ab985b7b9d24d2eefa950
Jarek Miszczak <miszczak@iitis.pl>
2022-12-16 13:01:13 +0100

    Improved experiment management

--- make_links.sh
+++ make_links.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for sn in `cat experiments.xml | grep -Po "(?<=name=\")[_a-z0-9-]*" `; do 
   echo "[INFO] Creating link for experiment $sn"
-	ln -sf ./run.sh $sn.sh ;
+	ln -sf ./run.sh $sn.sh
+  chmod +x $sn.sh
 done
