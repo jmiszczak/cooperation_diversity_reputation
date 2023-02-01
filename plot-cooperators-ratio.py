52444d8e17a3500df08b8245d750585975b69f25
Jarek Miszczak <jarek@miszczak.eu>
2023-02-01 15:17:51 +0100

    Started plotting routine

--- plot-cooperators-ratio.py
+++ plot-cooperators-ratio.py
@@ -18,3 +18,39 @@ mpl.rc('font', size=10)
 exp_desc = 'cooperators-diversity'
 
 data = pd.read_csv(exp_desc + '.csv', header=6)
+
+#sxs = { 'vN' : "von Neumann", 'M': 'Moore', 'rvN' : 'random von Neumann', 'rM': 'random Moore'} #, 'rvNM': 'random von Neumann or random  Moore' }
+sxs = { 'vN' : "von Neumann"}
+
+markers = ['o', 'x', 's', '^', '2']
+colors = ['k--', 'r-.', 'b:', 'g-', 'm']
+
+# %% calculate plotted data
+
+# [
+#  '[run number]', 'noise-factor', 'synergy-factor', 'neighborhood-type',
+#   'world-size', '[step]', 'cooperators-fraction', 'mean-cooperators1k'
+#   ]
+
+v = [ 'synergy-factor', 'neighborhood-type', 'world-size', 'mean-cooperators-fraction' ]
+
+# NOTE: start with some subset of data
+var0s = data['synergy-factor'].unique()
+var1s = data['neighborhood-type'].unique()[:1]
+var2s = data['world-size'].unique()[:1]
+
+df = pd.DataFrame(columns=v)
+
+for v0 in var0s:
+    for v1 in var1s:
+        for v2 in var2s: 
+          df.loc[len(df.index)] = [
+                v0,
+                v1,
+                v2,
+                data[(data[v[0]] == v0) & (data[v[1]] == v1) & (data[v[2]] == v2)]['cooperators-fraction'].mean()
+            ]
+          
+          
+# %% plot data
+
